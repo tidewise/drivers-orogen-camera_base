1a6effb7cc7da9b6ee6123fc3cedfebcdef46f6a
seixasxbr <mateus.seixas@tidewise.io>
2025-01-27 14:12:16 -0300

    chore: indent code

--- tasks/Preprocess.cpp
+++ tasks/Preprocess.cpp
@@ -6,14 +6,14 @@ using namespace camera_base;
 using namespace base::samples::frame;
 
 Preprocess::Preprocess(std::string const& name)
-    : PreprocessBase(name),
-    oframe(new Frame())
+    : PreprocessBase(name)
+    , oframe(new Frame())
 {
 }
 
 Preprocess::Preprocess(std::string const& name, RTT::ExecutionEngine* engine)
-    : PreprocessBase(name, engine),
-    oframe(new Frame())
+    : PreprocessBase(name, engine)
+    , oframe(new Frame())
 {
 }
 
@@ -26,16 +26,16 @@ Preprocess::~Preprocess()
 // documentation about them.
 bool Preprocess::configureHook()
 {
-    if (! PreprocessBase::configureHook())
+    if (!PreprocessBase::configureHook())
         return false;
-    if(_undistort.value())
+    if (_undistort.value())
         frame_helper.setCalibrationParameter(_calibration_parameters.value());
     return true;
 }
 
 bool Preprocess::startHook()
 {
-    if (! PreprocessBase::startHook())
+    if (!PreprocessBase::startHook())
         return false;
     return true;
 }
@@ -43,21 +43,24 @@ bool Preprocess::startHook()
 void Preprocess::updateHook()
 {
     PreprocessBase::updateHook();
-    RTT::extras::ReadOnlyPointer<base::samples::frame::Frame> iframe;	
-    if(_iframe.read(iframe) != RTT::NewData)
+    RTT::extras::ReadOnlyPointer<base::samples::frame::Frame> iframe;
+    if (_iframe.read(iframe) != RTT::NewData)
         return;
 
-    Frame *frame_ptr = oframe.write_access();
-    frame_ptr->init(iframe->size.width*_scale_x-_offset_x,
-            iframe->size.height*_scale_y-_offset_y,
-            iframe->getDataDepth(),_format);
-    try
-    {
-        frame_helper.convert(*iframe,*frame_ptr,_offset_x.value(),
-                             _offset_y.value(),_resize_algorithm.value(),_undistort.value());
+    Frame* frame_ptr = oframe.write_access();
+    frame_ptr->init(iframe->size.width * _scale_x - _offset_x,
+        iframe->size.height * _scale_y - _offset_y,
+        iframe->getDataDepth(),
+        _format);
+    try {
+        frame_helper.convert(*iframe,
+            *frame_ptr,
+            _offset_x.value(),
+            _offset_y.value(),
+            _resize_algorithm.value(),
+            _undistort.value());
     }
-    catch(std::runtime_error e)
-    {
+    catch (std::runtime_error e) {
         RTT::log(RTT::Error) << "processing error: " << e.what() << RTT::endlog();
         if (state() != PROCESSING_ERROR) {
             state(PROCESSING_ERROR);
